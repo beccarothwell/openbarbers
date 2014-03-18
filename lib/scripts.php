863daab83f9dfc9f237cccddc9c5f5fd8dce39b9
beccarothwell <becca.rothwell@gmail.com>
2014-03-18 01:55:15 +0000

    navbar fixed to top and book now button added

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '369c8a8025fd439f1c71f6567b1d5148');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8e028ffdf0cf9fae0227fe13b2935f12');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -11,11 +11,13 @@
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
+	<button type="button" class="btn btn-default navbar-btn navbar-right navbar-link">Book Now</button>
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
     </nav>
+	
   </div>
 </header>
